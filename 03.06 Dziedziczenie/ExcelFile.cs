9d4fa9a9c456aec5cb3c651d9b06652b1219bde1
Muniox <muniox20@gmail.com>
2024-08-22 23:17:33 +0200

    Add class ExcelFile

---
+++ 03.06 Dziedziczenie/ExcelFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._06_Dziedziczenie
+{
+    internal class ExcelFile
+    {
+        public string FileName { get; set; }
+        public int Size { get; set; }
+    }
+}
--- 03.06 Dziedziczenie/Program.cs
+++ 03.06 Dziedziczenie/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person bill = new Person("Bill", "Wick");
+
+            bill.SetDateOfBirth(new DateTime(1990, 1, 1));
+
+            bill.SayHi();
+            bill.ContactNumber = "9998887776";
+            Console.WriteLine(bill.ContactNumber);
+
+            Person john = new Person(new DateTime(1990, 1, 2), "John", "Wick");
+
+            john.SayHi();
+
+            Console.WriteLine($"Pbjects of Person type count: {Person.Count}");
         }
     }
 }
