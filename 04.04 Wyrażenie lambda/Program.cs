20889340d9690d2a6146f36af8077fdd319f247a
Muniox <muniox20@gmail.com>
2024-08-25 21:47:54 +0200

    04.05 Dictionary

---
+++ 04.04 Wyrażenie lambda/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04._04_Wyrażenie_lambda
+{
+    internal class Program
+    {
+        static List<Person> GetEmployess()
+        {
+            List<Person> employess = new List<Person>()
+            {
+                new Person(new DateTime(1990, 2, 2), "Bill", "Wick"),
+                new Person(new DateTime(1995, 6, 3), "John", "Wick"),
+                new Person(new DateTime(1996, 4, 3), "Bob", "Wick"),
+                new Person(new DateTime(2001, 2, 2), "Bill", "Smith"),
+                new Person(new DateTime(2000, 2, 2), "John", "Smith"),
+                new Person(new DateTime(2005, 2, 2), "Bob", "Smith"),
+                new Person(new DateTime(2003, 2, 2), "Ed", "Smith"),
+            };
+
+            return employess;
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<Person> employess = GetEmployess();
+
+
+            List<Person> youngEmployees = employess.Where(e => e.GetDateOfBirth() > new DateTime(2000, 1 ,1)).ToList();
+
+            Console.WriteLine($"Young employess count {youngEmployees.Count}");
+
+            Person? bob = youngEmployees.FirstOrDefault(e => e.FirstName == "Bob");
+
+            if (bob != null)
+            {
+                bob.SayHi();
+            }
+            else
+            {
+                Console.WriteLine("Bob not found!");
+            }
+        }
+    }
+}
---
+++ 04.05 Słownik(Dictionary)/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04._05_Słownik_Dictionary_
+{
+    internal class Program
+    {
+        static List<Currency> GetCurrencies()
+        {
+            return new List<Currency>
+            {
+                new Currency("usd", "United States, Dollar", 1),
+                new Currency("eur", "Euro", 0.83975),
+                new Currency("gbp", "British Pund", 0.74771),
+                new Currency("cad", "Canadian Dollar", 1.30724),
+                new Currency("inr", "Indian Rupee", 73.04),
+                new Currency("mxn", "Mexican Peso", 21.7571)
+            };
+        }
+        static void Main(string[] args)
+        {
+            List<Currency> currencies = GetCurrencies();
+            Console.WriteLine("Check the rate for:");
+            string? userInpurt = Console.ReadLine();
+
+            Currency? selectedCurrency = currencies.FirstOrDefault(c => c.Name == userInpurt);
+            if(selectedCurrency != null)
+            {
+                Console.WriteLine($"Rate for USD to {selectedCurrency.Name} is {selectedCurrency.Rate}");
+            }
+            else
+            {
+                Console.WriteLine("Currency not found");
+            }
+        }
+    }
+}
