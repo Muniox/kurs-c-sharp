2c451c2cd694e6bb70b2caef7d19a5eba1d65cb5
Muniox <muniox20@gmail.com>
2024-09-19 21:36:09 +0200

    Obsługa plików tekstowych

---
+++ 05.04 Słowa kluczowe const & readonly/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _05._04_Słowa_kluczowe_const___readonly
+{
+    internal class Program
+    {
+        class ApiClient
+        {
+            // w readonly zmienną możemy zmienić jedynie w konstruktorze
+            private readonly string baseUrl = "https://our-api-dev.com";
+            private const string getUsersEndpoint = "/api/users";
+            private readonly HttpClient httpClient = new HttpClient();
+            private int defaultPort = 80;
+
+
+            //wartości referencyjne mogą być tylko readonly
+            //const List<string> users = new List<string>(); // bad
+            //readonly List<string> users = new List<string>(); // good :)
+
+            public ApiClient(string _baseUrl)
+            {
+                baseUrl = _baseUrl;
+            }
+
+            public void GetUsers()
+            {
+                //httpClient = new HttpClient(); //proba nadpisania spowoduje blad
+
+                var getUsersUri = $"{baseUrl}{getUsersEndpoint}";
+
+                //getUsersEndpoint = "/api/user"; // może być nadpisane jeśli nie mamy readonly albo const
+
+                // HTTP GET Request @getUsersUri
+                httpClient.GetAsync(getUsersUri);
+                // return users
+            }
+
+            public void PostUser()
+            {
+
+            }
+
+            public void GetResources()
+            {
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 06.01 Obsługa plików tekstowych/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._01_Obsługa_plików_tekstowych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
