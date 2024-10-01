ccd5d20857caa933c85f49c0e5174467c375cb80
pawel.bartoszewski <pawel.bartoszewski@lge.com>
2024-10-01 13:56:54 +0200

    06.03 Wyrażenie using

---
+++ 06.03 Wyrażenie using/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _06._03_Wyrażenie_using;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var filePath = $@"{AppDomain.CurrentDomain.BaseDirectory}\..\..\..\..\TextFiles\File\file.txt";
+        var fileContent = File.ReadAllLines(filePath);
+
+        using (var readFileStream = new FileStream(filePath, FileMode.Open))
+        {
+            // readFileStream.Read();
+            // ...
+            // throw new Exception();
+        } // blok using mozemy wykonac dla kazdej klasy ktora dziedziczy po IDisposable
+
+
+
+        // należy zamknąć stream przed jego ponownym wykorzystaniem
+        // readFileStream.Close();
+
+        var writeFileStream = new FileStream(filePath, FileMode.Open);
+
+        // writeFileStream.Write();
+        // readFileStream.Close();
+    }
+}
