3dc7c9e2a32af803679ac71ede5699983ef22bf1
pawel.bartoszewski <pawel.bartoszewski@lge.com>
2024-10-02 13:42:25 +0200

    Struktura DateTime

---
+++ 07.03 Struktura DateTime w .NET/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07._03_Struktura_DateTime_w_.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
